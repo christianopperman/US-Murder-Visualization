e4786cf05634de87277b72de7ff575efd2f3893e
christianopperman <christian.opperman@gmail.com>
2020-01-13 21:55:11 -0500

    Attempted to add filtering tree to server.R. Doesn't work

--- server.R
+++ server.R
@@ -1,49 +1,99 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  
   murder_map_df = reactive({
-    
-    #Filter dataset by victim gender as selected by the user in the genderCheckGroup
-    if(is.null(input$ageCheckGroup)){
-      murder_map_df = murder_database
+    if (!is.null(input$genderCheckGroup)) {
+      #Check to see if there are inputs in the gender selection boxes
+      if (!is.null(input$ageCheckGroup)) {
+        #Check to see if there are inputs in the age selection boxes
+        if (!is.null(input$methodCheckGroup)) {
+          #Check to see if there are inputs in the method selection boxes
+          #Gender, age, and method selection exist
+          murder_map_df = murder_database %>%
+            filter(
+              .,
+              `Victim Sex` %in% input$genderCheckGroup &
+                `Victim Age` %in% input$ageCheckGroup &
+                `Weapon` %in% input$methodCheckGroup
+            )
+        } else {
+          #Gender and age selection exist, method does not
+          murder_map_df = murder_database %>%
+            filter(
+              .,
+              `Victim Sex` %in% input$genderCheckGroup &
+                `Victim Age` %in% input$ageCheckGroup
+            )
+        }
+      } else {
+        #Gender selection exists, age does not; check for method
+        if (!is.null(input$methodCheckGroup)) {
+          # Gender and method selection exist, age does not
+          murder_map_df = murder_database %>%
+            filter(
+              .,
+              `Victim Sex` %in% input$genderCheckGroup &
+                `Weapon` %in% input$methodCheckGroup
+            )
+        } else {
+          #Gender selection exists, age and method do not
+          murder_map_df = murder_database %>%
+            filter(., `Victim Sex` %in% input$genderCheckGroup)
+        }
+      }
     } else {
-      murder_map_df = murder_database %>% filter(., `Victim Age` %in% input$ageCheckGroup)
+      #Gender selection doesn't exist; check for age and method
+      if (!is.null(input$ageCheckGroup)) {
+        #Age selection exists, gender does not; check for method
+        if (!is.null(input$methodCheckGroup)) {
+          #Age and method selection exist, gender does not
+          murder_map_df = murder_database %>%
+            filter(
+              .,
+              `Victim Age` %in% input$ageCheckGroup &
+                Weapon %in% input$methodCheckGroup
+            )
+        } else {
+          #Age selection exists, gender and method do not
+          murder_map_df = murder_database %>%
+            filter(., `Victim Age` %in% input$ageCheckGroup)
+        }
+      } else {
+        #Gender and age selection don't exist; check for method
+        if (!is.null(input$methodCheckGroup)) {
+          murder_map_df = murder_database %>%
+            filter(., `Weapon` %in% input$methodCheckGroup)
+        } else{
+          murder_map_df = murder_database
+        }
+      }
     }
-    
-    #Filter dataset by victim age as selected by the user in the ageCheckGroup
-    #if(is.null(input$ageCheckGroup)){
-     # murder_map_df = murder_map_df
-    #} else {
-    #  murder_map_df = murder_map_df %>% filter(., `Victim Age` %in% input$ageCheckGroup)
-    #}
     
     #Filter dataset by year as selected by the user in the yearSlider
     #Group dataset by State and create outputs to be used to color the map
-    murder_map_df = 
-      murder_map_df %>% 
-      filter(., between(Year, input$yearSlider[1], input$yearSlider[2])) %>% 
-      group_by(., State, `Victim Age`) %>% 
-      summarise(., log_murders=log(sum(Incident)), total_murders = sum(Incident))
-    
-    #murder_map_df = 
-      #murder_database %>% 
-      #filter(., between(Year, input$yearSlider[1], input$yearSlider[2])) %>% 
-      #filter(., `Victim Sex` %in% ifelse(is.null(input$genderCheckGroup), victim.genders, input$genderCheckGroup)) %>% 
-      #group_by(., State) %>% 
-      #summarise(., log_murders = log(sum(Incident)), total_murders = sum(Incident))
+    murder_map_df =
+      murder_map_df %>%
+      filter(., between(Year, input$yearSlider[1], input$yearSlider[2])) %>%
+      group_by(., State) %>%
+      summarise(.,
+                log_murders = log(sum(Incident)),
+                total_murders = sum(Incident))
   })
   
   output$map = renderGvis({
-    gvisGeoChart(murder_map_df(),
-                 locationvar = "State",
-                 colorvar = "total_murders",
-                 #hovervar = "total_murders",
-                 options=list(region="US",
-                              displayMode="regions",
-                              resolution="provinces",
-                              width="auto", height="auto",
-                              colorAxis = "{colors: ['white', 'red']}")
-                 )
+    gvisGeoChart(
+      murder_map_df(),
+      locationvar = "State",
+      colorvar = "total_murders",
+      #hovervar = "total_murders",
+      options = list(
+        region = "US",
+        displayMode = "regions",
+        resolution = "provinces",
+        width = "auto",
+        height = "auto",
+        colorAxis = "{colors: ['white', 'red']}"
+      )
+    )
   })
 })
