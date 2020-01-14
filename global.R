5616047baf36663c425a6fb49ff3ee1c07c074e6
christianopperman <christian.opperman@gmail.com>
2020-01-14 15:30:54 -0500

    Changed age filtering conditions to match decades of age; fixed filtering conditions to correct bug where victims between ages 59-60 weren't represented

--- global.R
+++ global.R
@@ -9,12 +9,13 @@ statepop_by_year = fread(file="~/Desktop/NYCDSA/Projects/ShinyMurderApp/state_po
 
 murder_database = murder_database %>% 
   mutate(., `Victim_Age_Category` = 
-           ifelse(`Victim Age`<=10, "0-10",
-                  ifelse(`Victim Age`<=20, "11-20",
-                         ifelse(`Victim Age`<=30, "21-30",
-                                ifelse(`Victim Age`<=40, "31-40",
-                                       ifelse(`Victim Age`<=50, "41-50",
-                                              ifelse(`Victim Age`<=60, "61-70", "70+")))))))
+           ifelse(`Victim Age`<=9, "0-9",
+                  ifelse(`Victim Age`<=19, "10-19",
+                         ifelse(`Victim Age`<=29, "20-29",
+                                ifelse(`Victim Age`<=39, "30-39",
+                                       ifelse(`Victim Age`<=49, "40-49",
+                                              ifelse(`Victim Age`<=59, "50-59",
+                                                    ifelse(`Victim Age` <= 69, "60-69", "70+"))))))))
 
 murder_database = inner_join(murder_database, statepop_by_year, by = c("State", "Year")) %>%
   mutate(., `Total Victims` = `Victim Count` + 1) %>% 
@@ -25,5 +26,5 @@ state_stat <- data.frame(state.name = rownames(state.x77), state.x77)
 
 #Define choice selections for map visualization
 victim.genders = unique(murder_database$`Victim Sex`)
-victim.ages = c('0-10', '11-20', '21-30', '31-40','41-50', '51-60', '61-70', '70+')
+victim.ages = c('0-9', '10-19', '20-29', '30-39','40-49', '50-59', '60-69', '70+')
 murder.methods = sort(unique(murder_database$Weapon))
--- server.R
+++ server.R
@@ -15,9 +15,9 @@ shinyServer(function(input, output) {
       filter(., `Weapon` %in% if(is.null(input$methodCheckGroup)){murder.methods} else {input$methodCheckGroup}) %>%
       filter(., State !="District of Columbia") %>% 
       group_by(., State) %>% 
-      summarise(., totalvictims = sum(`Total Victims`), 
-                victimsper1kpeople = sum(`Total Victims`)/mean(Population.in.1000s), 
-                roundedvictimsper1kpeople = round(sum(`Total Victims`)/mean(Population.in.1000s)))
+      summarise(., totalvictims = sum(`Total Victims`, na.rm = TRUE), 
+                victimsper1kpeople = sum(`Total Victims`, na.rm = TRUE)/mean(Population.in.1000s, na.rm = TRUE), 
+                roundedvictimsper1kpeople = round(sum(`Total Victims`, na.rm = TRUE)/mean(Population.in.1000s, na.rm = TRUE)))
   })
   
   #Output a map color coded by the total number of murders committed that match the criteria as selected
@@ -36,16 +36,18 @@ shinyServer(function(input, output) {
   
   #Output an infobox providing the U.S. state that matches the criteria as selected with the most murders
   output$maxBox = renderInfoBox({
-    max_state = murder_map_df()$State[murder_map_df()$roundedvictimsper1kpeople == max(murder_map_df()$roundedvictimsper1kpeople)]
-    infoBox(title = "Most Murderous State", subtitle = "(Murders per 1,000 people)", 
-            value = max_state, icon = icon("long-arrow-alt-up"), color = "black", width = "100%"
+    max_state = murder_map_df()$State[murder_map_df()$victimsper1kpeople == max(murder_map_df()$victimsper1kpeople)]
+    infoBox(title = "Most Murderous State", 
+            subtitle = paste("(", round(max(murder_map_df()$victimsper1kpeople), digits = 2), " murders per 1,000 people)", sep =""),
+            value = max_state, color = "black", width = "100%",
+            icon = icon("long-arrow-alt-up")
             )
   })
   
   #Output an infobox providing the U.S. state that matches the criteria as selected with the most murders
   output$avgBox = renderInfoBox({
-    avg_murders = round(mean(murder_map_df()$roundedvictimsper1kpeople), digits = 2)
-    infoBox(title = "Average Murders", subtitle = "(Murders per 1,000 people)", 
+    avg_murders = round(mean(murder_map_df()$victimsper1kpeople), digits = 2)
+    infoBox(title = "Average Murders per 1,000 people", 
             value = avg_murders, icon = icon("balance-scale"), color = "black", width = "100%"
             )
   })
